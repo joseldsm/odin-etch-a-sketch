958169fde7bb551aad0c68eae1ba6d5694d13cba
joseldsm <165505426+joseldsm@users.noreply.github.com>
2024-09-09 11:10:21 +0200

    Project setup and grid 16x16 created in row direction

---
+++ script.js
@@ -0,0 +1,14 @@
+function createGrid(gridSize) {
+    const flexBasis = 1/(gridSize+1)*100;
+
+    for (let i = 0; i < (gridSize*gridSize); i++) {
+        const div = document.createElement("div");
+        div.textContent = i+1;
+        container.appendChild(div);
+        div.style.flexBasis = `${flexBasis}%`;
+    }
+}
+
+const container = document.querySelector("#container");
+createGrid(16);
+
